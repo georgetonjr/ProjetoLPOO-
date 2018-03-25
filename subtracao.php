f5dbd7e9ee791237c7e89067db4ba494cb7e3297
georgetonjr <georgetonjr@gmail.com>
2018-03-25 10:29:26 -0300

    add subtracao,divisao

--- subtracao.php
+++ subtracao.php
@@ -6,17 +6,17 @@
 <body>
 	<div style="text-align: center;">
 		<form method="post" action="subtracao.php">
-			numero 1: <input type="text" name="Número3"><br>
-			numero 2: <input type="text" name="Número4"><br>
+			numero 1: <input type="text" name="numero1"><br>
+			numero 2: <input type="text" name="numero2"><br>
 			<input type="submit" name="submit" value="Subtrair"><br>
 			<input type="reset" name="reset" value="limpar"><br>
 			<input onclick="location.href = 'index.php' " type="button" value="Pagina Inicial">
 		</form>
 		<?php
 			if (!empty($_POST['submit'])){
-				$n3 = $_POST['numero3'];
-				$n4 = $_POST['numero4'];
-				$res = $n3-$n4;
+				$n1 = $_POST['numero1'];
+				$n2 = $_POST['numero2'];
+				$res = $n1-$n2;
 				echo ('Resultado: '. $res);
 			}
 		?>
